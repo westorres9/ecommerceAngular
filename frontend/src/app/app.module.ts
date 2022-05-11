0309a528ccd93f07aeb4d272f73d9d6f0129a974
westorres9 <wester.torres9@gmail.com>
2022-05-11 00:04:44 -0300

    roteamento e APP_BASE_HREF

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navigation/menu/menu.component';
@@ -7,6 +9,7 @@ import { HomeComponent } from './navigation/home/home.component';
 import { FooterComponent } from './navigation/footer/footer.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
+import { rootRouterConfig } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -18,9 +21,12 @@ import { ContatoComponent } from './institucional/contato/contato.component';
     ContatoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(rootRouterConfig, {useHash: false})]
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
